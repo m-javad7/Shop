edb2faa60bb2af879abd971e4fed169285ed22fd
m-javad7 <m.javad7721@gmail.com>
2024-01-21 19:57:51 +0330

    shop

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -7,14 +7,16 @@ export const Cart =() => {
     const {cartItems , resetCart} = useContext(ShopContext)
     return(
         <>
+        <div className="container">
         <h1>Your cart items</h1>
-        <div className="row">
+        <div className="row justify-content-center">
             {PRODUCTS.map((p)=>{
                 if( cartItems?.some((i)=> i.id===p.id && i.count >0))
                  return <Product data={p} ></Product>
             })}
         </div>
         <button className="btn btn-warning m-3" onClick={resetCart}>reset</button>
+        </div>
         </>
     )
     }
